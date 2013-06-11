e92441d8990439735632719142aee1aaba8add15
Dimitri Kurashvili <dimakura@gmail.com>
2013-06-11 13:15:03 +0400

    fallback locale

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module TelasiGe
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ka
+    config.i18n.fallbacks = true
+
   end
 end
