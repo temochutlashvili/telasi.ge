abcafe642330ade52f023ee0407c2f416fd39e45
Dimitri Kurashvili <dimakura@gmail.com>
2013-06-18 13:47:21 +0400

    base display for user

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,5 +2,6 @@
 class Admin::BaseController < Admin::AdminController
   def index
     @title = 'ადმინისტრირება'
+    @users = Sys::User.desc(:_id).paginate(per_page: 15, page: params[:page])
   end
 end
