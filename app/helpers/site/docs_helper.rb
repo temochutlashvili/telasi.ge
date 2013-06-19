9193f4c5324e3a40cbf0b0f019e0896f2dfd5bb7
Dimitri Kurashvili <dimakura@gmail.com>
2013-06-19 15:22:08 +0400

    laws page

--- app/helpers/site/docs_helper.rb
+++ app/helpers/site/docs_helper.rb
@@ -3,7 +3,7 @@ module Site::DocsHelper
   include Forma::Html
 
   def downloads_table(docs)
-    el('table', attrs: { class: 'table' }, children: [
+    el('table', attrs: { class: 'table table-striped table-hover' }, children: [
       el('tbody', children: docs.map { |doc|
         ext = doc[:url][-3..-1]
         el('tr', children: [
@@ -11,7 +11,7 @@ module Site::DocsHelper
           el('td', attrs: { width: 200 }, children: [
             el('a', attrs: { href: doc[:url], class: 'download' }, children: [
               el('i', attrs: { class: 'icon-download' }),
-              el('span', text: "#{ext.upcase} #{t('site.general.format')}")
+              el('span', text: " #{ext.upcase} #{t('site.general.format')}")
             ])
           ])
         ])
