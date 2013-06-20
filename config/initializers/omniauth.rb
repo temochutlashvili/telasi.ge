6f3bcd006d3ec862d4f16fe8ec9f31056fa16c7c
Dimitri Kurashvili <dimakura@gmail.com>
2013-06-20 11:44:01 +0400

    remove omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-end
