570f9ccd2f12a4f2c77de7a12a1d883db2ec9631
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-17 12:12:19 +0400

    profile page: initial version

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,16 @@
 # -*- encoding : utf-8 -*-
 class ProfileController < ApplicationController
   def index
+    @title = I18n.t('models.sys_user.actions.profile')
+    @user = current_user
+  end
+
+  protected
+
+  def nav
+    [
+      { label: I18n.t('menu.profile'), url: profile_url,  },
+      { label: I18n.t('models.sys_user.actions.change_password'), url: change_password_url, active: ( action_name == 'change_password' ) },
+    ]
   end
 end
