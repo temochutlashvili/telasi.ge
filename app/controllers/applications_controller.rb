3f1abd941de3efc70ff3a070adb6e5ff9d8afdcb
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-18 12:30:56 +0400

    changes in translation

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,7 +2,7 @@ class ApplicationsController < ApplicationController
   before_action :validate_login, :nav
 
   def index
-    @title = I18n.t('menu.applications')
+    @title = I18n.t('menu.applications_alt')
   end
 
   protected
