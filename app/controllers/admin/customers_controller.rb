3fee0744379bdd9f96a2d706b06217a900ea41a8
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-19 18:01:51 +0400

    registration properties

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,4 +4,9 @@ class Admin::CustomersController < Admin::AdminController
     @title = I18n.t('applications.admin.customers_title')
     @registrations = Billing::CustomerRegistration.desc(:_id).paginate(page: params[:page], per_page: 10)
   end
+
+  def show
+    @title = I18n.t('models.billing_customer_registration.title.single')
+    @registration = Billing::CustomerRegistration.find(params[:id])
+  end
 end
