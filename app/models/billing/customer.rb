c5f3db48fcf38af89e3e3dbfc60e2c1bfba2cfa0
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-20 16:53:49 +0400

    show cut warning

--- app/models/billing/customer.rb
+++ app/models/billing/customer.rb
@@ -24,24 +24,21 @@ class Billing::Customer < ActiveRecord::Base
   def trash_balance; self.trash_customer.balance rescue 0 end
   def last_bill_date; self.item_bills.last.billdate end
   def last_bill_number; self.item_bills.last.billnumber end
+  def cut_deadline; self.item_bills.last.lastday end
 
-  def pre_payment
-    Billing::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).inject(0) { |sum, payment| sum += payment.amount }
+  def cut_candidate?
+    if self.except then false
+    else self.balance > 0.5 or self.trash_balance > 0.5 or self.current_water_balance > 0.5 end
   end
 
-  def pre_payment_date
-    Billing::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).order('paykey desc').first.paydate rescue nil
-  end
-
+  def pre_payment; Billing::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).inject(0) { |sum, payment| sum += payment.amount } end
+  def pre_payment_date; Billing::Payment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).order('paykey desc').first.paydate rescue nil end
   def pre_water_payment
     Billing::WaterPayment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).inject(0) do |sum, payment|
       sum += payment.amount
     end
   end
-
-  def pre_water_payment_date
-    Billing::WaterPayment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).order('paykey desc').first.paydate rescue nil
-  end
+  def pre_water_payment_date; Billing::WaterPayment.where('paydate > ? AND custkey = ? AND status = 1', Date.today - 7, self.custkey).order('paykey desc').first.paydate rescue nil end
 
   def status_name
     case self.statuskey
