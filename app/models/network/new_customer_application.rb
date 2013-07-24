d208b0442a2b4d9af706ceabeddbcb75860484d9
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-24 12:50:16 +0400

    adding status to NewCustomerApplication

--- app/models/network/new_customer_application.rb
+++ app/models/network/new_customer_application.rb
@@ -1,5 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Network::NewCustomerApplication
+  STATUS_DEFAULT    = 0
+  STATUS_SENT       = 1
+  STATUS_CANCELED   = 2
+  STATUS_CONFIRMED  = 3
+  STATUS_COMPLETE   = 4
   include Mongoid::Document
   include Mongoid::Timestamps
   belongs_to :user, class_name: 'Sys::User'
@@ -12,6 +17,7 @@ class Network::NewCustomerApplication
   field :address,   type: String
   field :bank_code,    type: String
   field :bank_account, type: String
+  field :status,     type: Integer, default: STATUS_DEFAULT
   validates :user, presence: { message: 'user required' }
   validates :rs_tin, presence: { message: I18n.t('models.network_new_customer_application.errors.tin_required') }
   validates :mobile, presence: { message: I18n.t('models.network_new_customer_application.errors.mobile_required') }
@@ -25,6 +31,7 @@ class Network::NewCustomerApplication
   private
 
   def assign_number; self.number = (Network::NewCustomerApplication.last.number rescue 1) end
+  def status_name; I18n.t("models.network_new_customer_application.status.#{self.status}") end
 
   def validate_rs_name
     if self.rs_tin.present?
