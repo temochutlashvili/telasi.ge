7a7f38a1b3648dd020d60d276064edad16200bec
Dimitri Kurashvili <dimakura@gmail.com>
2013-07-26 11:50:56 +0400

    show new network application

--- app/controllers/admin/network_controller.rb
+++ app/controllers/admin/network_controller.rb
@@ -7,6 +7,11 @@ class Admin::NetworkController < Admin::AdminController
     @applications = Network::NewCustomerApplication.desc(:_id)
   end
 
+  def new_customer
+    @title = 'ქსელზე მიერთების განცხადება'
+    @application = Network::NewCustomerApplication.find(params[:id])
+  end
+
 # ==> Tariffs
 
   # TODO: tariffs page
@@ -23,8 +28,9 @@ class Admin::NetworkController < Admin::AdminController
   private
 
   def resolve_layout
-    case action_name
-    when 'index' then 'one_column'
-    else 'two_column' end
+    # case action_name
+    # when 'index' then 'one_column'
+    # else 'two_column' end
+    'one_column'
   end
 end
