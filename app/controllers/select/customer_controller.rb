3919e3517ea8273ab27364a371139162e915b8c2
Dimitri Kurashvili <dimakura@gmail.com>
2013-08-08 12:56:10 +0400

    search customer

--- app/controllers/select/customer_controller.rb
+++ app/controllers/select/customer_controller.rb
@@ -4,7 +4,12 @@ class Select::CustomerController < ApplicationController
 
   def index
     @title = 'აბონენტის არჩევა'
-    @customers = Billing::Customer.paginate(per_page: 10, page: params[:page])
-    # TODO: search
+    @search = params[:search] == 'clear' ? nil : params[:search]
+    @customers = Billing::Customer
+    if @search
+      @customers = @customers.where('accnumb like ?', @search[:accnumb].likefy) if @search[:accnumb].present?
+      @customers = @customers.where('custname like ?', @search[:custname].likefy.to_geo) if @search[:custname].present?
+    end
+    @customers = @customers.paginate(per_page: 10, page: params[:page])
   end
 end
