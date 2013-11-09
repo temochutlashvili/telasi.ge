a986630656924f8ec5305b0c9b05e4076bee63fe
Dimitri Kurashvili <dimakura@gmail.com>
2013-11-09 15:04:30 +0400

    user admin in internal layout

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Admin::UsersController < Admin::AdminController
+  layout 'internal'
+
   def index
     @title = I18n.t('applications.admin.users_title')
     @users = Sys::User.desc(:_id).paginate(page: params[:page], per_page: 10)
@@ -17,6 +19,14 @@ class Admin::UsersController < Admin::AdminController
       if @user.update_attributes(params.require(:sys_user).permit(:first_name, :last_name, :mobile, :admin, :email_confirmed))
         redirect_to admin_user_url(id: @user.id), notice: I18n.t('models.sys_user.actions.edit_complete')
       end
+    end
+  end
+
+  def nav
+    @nav = { 'მომხმარებლები' => admin_users_url }
+    if @user
+      @nav[@user.full_name] = admin_user_url(id: @user.id)
+      @nav[@title] = nil unless action_name == 'show'
     end
   end
 end
