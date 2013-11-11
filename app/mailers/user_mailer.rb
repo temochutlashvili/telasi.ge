7fdd613a3601472fa375e9baf65cb8d7df7450d0
Dimitri Kurashvili <dimakura@gmail.com>
2013-11-11 12:57:37 +0400

    localize messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class UserMailer < ActionMailer::Base
-  default :from => "my.telasi.ge <sys@telasi.ge>"
+  default from: 'my.telasi.ge <sys@telasi.ge>'
 
   #
   # მომხმარებელზე დადასტურების კოდის გაგზავნა.
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
   def email_confirmation(user)
     @user = user
     @url = confirm_url(host: Telasi::HOST, c: @user.email_confirm_hash, id: @user.id)
-    mail(to: "#{@user.full_name} <#{@user.email}>", subject: I18n.t('models.sys_user.actions.confirm_email'))
+    mail(to: "#{@user.full_name} <#{@user.email}>", subject: I18n.t('models.sys_user.actions.confirm_email'), locale: I18n.locale)
   end
 
   #
@@ -25,6 +25,6 @@ class UserMailer < ActionMailer::Base
     @user = user
     @user.generate_restore_hash!
     @url = restore_password_url(host: Telasi::HOST, id: @user.id, h: @user.password_restore_hash)
-    mail(to: "#{@user.full_name} <#{@user.email}>", subject: I18n.t('models.sys_user.actions.restore'))
+    mail(to: "#{@user.full_name} <#{@user.email}>", subject: I18n.t('models.sys_user.actions.restore'), locale: I18n.locale)
   end
 end
