d60edd48509758c7afcd94fc35b43c2b907ada40
Dimitri Kurashvili <dimakura@gmail.com>
2013-12-24 17:53:32 +0400

    show users for subscription controller

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Admin::SubscriptionsController < ApplicationController
   def index
+    @title = I18n.t('applications.admin.subscriptions')
     @subscriptions = Sys::Subscription.desc(:_id).paginate(page: params[:page], per_page: 20)
   end
 end
